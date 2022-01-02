816071a7f7708aabbb0455c7b760a2f32f098124
Michael Frankiewicz <90117951+michaelfranki@users.noreply.github.com>
2022-01-02 12:12:36 -0500

    Add files via upload

---
+++ PlayingCat.java
@@ -0,0 +1,18 @@
+package Chapter2.src;
+
+public class PlayingCat {
+    public static void main(String[] args) {
+        System.out.println(isCatPlaying(true, 10));
+        System.out.println(isCatPlaying(false, 36));
+        System.out.println(isCatPlaying(false, 35));
+    }
+
+    public static boolean isCatPlaying(boolean summer, int temperature) {
+        if(summer && (temperature  >= 25 && temperature <= 45)) {
+            return true;
+        } else if(!summer && (temperature >= 25 && temperature <= 35)) {
+            return true;
+        }
+        return false;
+    }
+}
